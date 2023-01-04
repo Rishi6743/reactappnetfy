02a1b37edd68a662351245a6b787f381c687e700
Rishi <rishipradhan896@gmail.com>
2023-01-04 22:05:17 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>This is Rishi's APP </h1>
+      <div className="logo">netPay</div>
         <form>
       <div className='login'>
           <input type="text" placeholder='Username'/>
