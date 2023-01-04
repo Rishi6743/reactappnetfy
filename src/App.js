f6e5774c689b0a69bddee899cf6f9857c53e8cdf
Rishi <rishipradhan896@gmail.com>
2023-01-04 21:28:15 +0530

    feature added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ function App() {
   return (
     <div className="App">
       <h1>This is Rishi's APP </h1>
+        <form>
+      <div className='login'>
+          <input type="text" placeholder='Username'/>
+          <input type="text" placeholder='Password'/>
+          <button>Log in</button>
+     </div>
+        </form>
     </div>
   );
 }
